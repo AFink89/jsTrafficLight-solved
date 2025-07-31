e999fa8dbbfe5c919f08e56c1d48e0241892691d
AMaupin <alexandrafink8989@gmail.com>
2025-07-31 08:52:54 -0700

    created if and else if statements, playing with ! && and || statements

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 var redLight = 'Red';
 var yellowLight = 'Yellow';
 var greenLight = 'Green';
-var currentLightColor 
+var currentLightColor = redLight;
+
+if (currentLightColor === greenLight) {
+  console.log("The light is Green");
+} else if (currentLightColor === yellowLight) {
+  console.log("The light is Yellow");
+} else if (currentLightColor === redLight) {
+  console.log("The light is Red");
+} else {
+  console.log("Invalid light color");
+}
+
+if (!(currentLightColor === redLight)) {
+  console.log("The light is NOT red");
+}
+
+if (currentLightColor === redLight || currentLightColor === yellowLight) {
+  console.log("Caution! Either stop or slow down.");
+}
+if (!(currentLightColor === greenLight) && (currentLightColor === yellowLight || currentLightColor === redLight)) {
+  console.log("You cannot go!");
+}
